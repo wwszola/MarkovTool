1e6a192f8fe68acf6bcf2f6683ab2f3dc6d656bc
wwszola <wwszola@student.agh.edu.pl>
2022-09-15 18:16:06 +0200

    print instead of comments

--- example2.py
+++ example2.py
@@ -3,16 +3,14 @@ from story import MarkovChain
 MarkovChain.reset_static_rng(0)
 
 chain = MarkovChain.random(
-    dimension=3, 
+    dimension=4, 
     iter_reset=True)
 
-# Some of these should be different from each other
+print('Beginning of these should be different from each other')
 for _ in range(3):
     print(chain.run(True))
 
-print('\n')
-
-# All of these should be the same
+print('All of these should be the same. Every iteration reset_static_rng is called')
 for _ in range(3):
     # Although iter_reset is True, call reset_static_rng to get identical first state
     MarkovChain.reset_static_rng()
