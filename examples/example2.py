5cc30317c817346fef708bc5db67b37d1c1d48c9
wwszola <wwszola@student.agh.edu.pl>
2022-09-22 17:53:24 +0200

    examples now follow new refactor architecture

--- examples/example2.py
+++ examples/example2.py
@@ -1,14 +1,14 @@
-from MarkovTool import Description, Variation, Endless
-from itertools import cycle     
+from MarkovTool import Description, Endless
 
-process = Description.random(4, seed_=0)
+
+process: Description = Description.random(4, seed_=0)
 
 print('Beginning of these should be different from each other')
 for _ in range(3):
     print(Endless(process).take(10))
 
-variation = Variation(process, {"initial_state": 1})
+variant = process.variant(initial_state = 0)
 
 print('All of these should be the same.')
 for _ in range(3):
-    print(Endless(variation).take(10))
+    print(Endless(variant).take(10))
--- examples/example3.py
+++ examples/example3.py
@@ -1,18 +1,19 @@
-from story import MarkovChain
+from MarkovTool import Description, Endless
 
-MarkovChain.normalize = True
-chain = MarkovChain.random(4, max_steps = 15, my_seed = 0, iter_reset = True)
-print('iter_reset=True')
-print('Original process:', chain.run(True), sep='\n')
 
-with chain.simulation(initial_state = 3) as chain1:
-    print('Process with set initial_state = 3:', chain1.run(True), sep='\n')
-    with chain1.simulation(my_seed = 7) as chain2:
-        print('Process with set initial state and new seed:', chain2.run(True), sep='\n')
+p1 = Description.random(4, seed_ = 1)
+p1.initial_state = 0
 
-new_matrix = chain.matrix
+f = Endless(p1)
+first_5_steps = f.take(5) 
+
+print('Original process:')
+print(first_5_steps, f.branch().take(10))
+
+print('The process branched at 5th step with forced _state=3:')
+print(first_5_steps, f.branch(_state = 3).take(10))
+
+print('The process branched at 5th step with no possible transitions to state 1:')
+new_matrix = p1.matrix
 new_matrix[:, 1] = 0.0
-new_initial_state = chain.initial_state
-new_initial_state[1] = 0.0
-with chain.simulation(matrix = new_matrix, initial_state = new_initial_state) as chain1:
-    print('Process with cut out all transitions to state 1:', chain1.run(True), sep='\n')
+print(first_5_steps, f.branch(matrix = new_matrix).take(10))
