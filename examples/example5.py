7879328662a05a62717f0f902180302a08fd536a
wwszola <wwszola@student.agh.edu.pl>
2022-10-29 17:23:36 +0200

    using Finite instance

--- examples/example5.py
+++ examples/example5.py
@@ -1,8 +1,6 @@
 from MarkovTool import *
 
-from numpy import identity, ones, triu, array
-from numpy.random import random
-from itertools import islice
+from numpy import identity, ones, triu
 
 source = Markov(
     dimension = 3, 
@@ -15,14 +13,9 @@ emmision1 = Description(
     my_seed = 2,
     matrix = triu(ones((3, 5)), 1)) # emitted state > input state always
 
-s = Endless(source) 
+s = Finite(source, lambda self: self._step > 10) 
 e1 = Dependent(emmision1, s)
 
-c = Collector(s, e1)
-
 # this should be called as a model
-for _ in islice(zip(s, e1), 0, 30):    
-    pass
-
-print(c._entries)
-c.close()
+for v1, v2 in zip(s, e1):    
+    print(v1, v2)
