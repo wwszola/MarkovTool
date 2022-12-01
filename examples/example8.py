7e3f4170943703c2c346b22af5a872346bdca881
wwszola <wwszola@student.agh.edu.pl>
2022-12-01 18:51:19 +0100

    different structure of a model

--- examples/example8.py
+++ examples/example8.py
@@ -1,18 +1,20 @@
 from MarkovTool import *
 from numpy import eye
 
-signal_desc = Markov(5).fill_random(2)
+
+# signal_mat = []
+signal_desc = Markov(4).fill_random(2)
 signal = Endless(signal_desc)
 
-activation_desc = Stochastic((5, 2)).fill_random(21)
+activation_desc = Stochastic((4, 2)).fill_random(21)
 activation = Dependent(activation_desc, signal)
 
 c = Collector(signal, activation)
 
 m = Model(
-    (signal, [1, 0]), 
-    (activation, [1, 1])
+    (signal, [1, 0, 0, 0]), 
+    (activation, [1, 1, 1, 1])
 )
 m.forward(32)
-print(*c.playback(signal), sep='   ')
+print(*c.playback(signal), sep='\t')
 print(*c.playback(activation))
